a128a21e3e3b5ad22d51f6509796c6f24d9e7aff
ltsikarlskoga-lgtm <ltsikarlskoga@gmail.com>
2026-02-12 13:47:25 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('lmx').then(c => c.addAll([
+    './index.html',
+    './manifest.json',
+    './service-worker.js'
+  ])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
